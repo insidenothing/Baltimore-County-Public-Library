e8065201b322fe4b2de8d7ebe5324de96bee51d2
Patrick McGuire <insidenothing@gmail.com>
2011-04-27 19:18:05 -0700

    bcc me for beta testing

--- cron.php
+++ cron.php
@@ -81,7 +81,7 @@ $subject = "Items out for BCPL.info";
 $headers = "MIME-Version: 1.0 \n";
 $headers .= "Content-type: text/html; charset=iso-8859-1 \n";
 $headers .= "From: insidenothing@gmail.com \n";
-//$headers .= "BCc: Patrick McGuire <insidenothing@gmail.com> \n";
+$headers .= "BCc: Patrick McGuire <insidenothing@gmail.com> \n";
 mail($d[email],$subject,$email,$headers);
 
 ?>
