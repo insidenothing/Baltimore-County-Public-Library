3594e0d553c2c11f8c30fc64a7b7e82abdcfae96
Patrick McGuire <insidenothing@gmail.com>
2011-04-27 17:58:21 -0700

    define url's

--- cron.php
+++ cron.php
@@ -2,5 +2,12 @@
 mysql_connect();
 mysql_select_db('bcpl');
 
+$loginURL = "https://catalog.bcpl.lib.md.us/polaris/logon.aspx";
+$overviewURL = "https://catalog.bcpl.lib.md.us/polaris/patronaccount/default.aspx";
+$listURL = "https://catalog.bcpl.lib.md.us/polaris/patronaccount/itemsout.aspx";
+
+
+
+
 
 ?>
