5030a45413da8ef38be288732d89dba37044b7d6
Patrick McGuire <insidenothing@gmail.com>
2011-04-27 18:35:51 -0700

    try this

--- cron.php
+++ cron.php
@@ -10,30 +10,23 @@ $overviewURL = "https://catalog.bcpl.lib.md.us/polaris/patronaccount/default.asp
 $listURL = "https://catalog.bcpl.lib.md.us/Mobile/MyAccount/ItemsOut";
 
 
-$link = "https://catalog.bcpl.lib.md.us/Mobile/MyAccount/Details/496142";
-
 
 $curl = curl_init();
 // Set options
 curl_setopt ($curl, CURLOPT_URL, $loginURL);
 curl_setopt ($curl, CURLOPT_TIMEOUT, '5');
 curl_setopt ($curl, CURLOPT_RETURNTRANSFER, '1');
+curl_setopt( $curl, CURLOPT_FOLLOWLOCATION, true );
 curl_setopt ($curl, CURLOPT_POSTFIELDS, 'barcodeOrUsername='.$d['user'].'&password='.$d['pass'].'&rememberMe=true');
 
 $buffer = curl_exec ($curl);
 
-curl_setopt ($curl, CURLOPT_URL, $link);
-curl_setopt ($curl, CURLOPT_TIMEOUT, '5');
-curl_setopt ($curl, CURLOPT_RETURNTRANSFER, '1');
-curl_setopt ($curl, CURLOPT_POSTFIELDS, 'barcodeOrUsername='.$d['user'].'&password='.$d['pass'].'&rememberMe=true');
-
-$buffer2 = curl_exec ($curl);
 
 curl_close ($curl);
 
 //if all goes well
 
-echo $buffer."<hr>".$buffer2;
+echo $buffer;
 
 
 
